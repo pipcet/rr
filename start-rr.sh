259a1e382bbfb1737ab67d0536b7a90e4f65fd27
Pip <pipcet@gmail.com>
2016-07-07 20:52:26 +0000

    enable continuous wrapping mode, see https://github.com/mozilla/rr/issues/1552#issuecomment-231203548

--- start-rr.sh
+++ start-rr.sh
@@ -1,8 +1,8 @@
 sudo modprobe msr
-sudo wrmsr -p 0 0xc0000105 0xff008000000e
-sudo wrmsr -p 1 0xc0000105 0xff018000000e
-sudo wrmsr -p 2 0xc0000105 0xff028000000e
-sudo wrmsr -p 3 0xc0000105 0xff038000000e
+sudo wrmsr -p 0 0xc0000105 0xff00e000000e
+sudo wrmsr -p 1 0xc0000105 0xff01e000000e
+sudo wrmsr -p 2 0xc0000105 0xff02e000000e
+sudo wrmsr -p 3 0xc0000105 0xff03e000000e
 echo 0 | sudo tee /proc/sys/kernel/perf_event_paranoid
 sudo mount -t cpuset cpuset /cpusets
 sudo mkdir /cpusets/one
