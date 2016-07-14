b8d237f72db0a947759aaa683d57463202782f47
Pip Cet <pipcet@gmail.com>
2016-07-14 22:04:14 +0000

    fix compilation

--- src/LWP.cc
+++ src/LWP.cc
@@ -1,93 +1,52 @@
+extern "C" {
+#include <sys/ptrace.h>
+#include <asm/ptrace.h>
+}
+#include <elf.h>
+#include "LWP.h"
+#include "PerfCounters.h"
+
+#include <asm/ldt.h>
+#include <assert.h>
+#include <err.h>
+#include <fcntl.h>
+#include <linux/perf_event.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+
+#include <algorithm>
+#include <string>
+
+#include "Flags.h"
+#include "kernel_metadata.h"
+#include "log.h"
+#include "util.h"
+#include "Task.h"
+
+#include "AddressSpace.h"
+#include "Event.h"
+#include "ExtraRegisters.h"
+#include "FdTable.h"
+#include "PerfCounters.h"
+#include "LWP.h"
+#include "PropertyTable.h"
+#include "Registers.h"
+#include "TaskishUid.h"
+#include "TraceStream.h"
+#include "WaitStatus.h"
+#include "kernel_abi.h"
+#include "kernel_supplement.h"
+#include "remote_code_ptr.h"
+#include "util.h"
+
 using namespace std;
 
 namespace rr {
-
-  class Task;
-
-  
-  class LWP {
-  public:
-    LWP(Task* task, pid_t tid);
-    ~LWP() {}
-
-    /**
-     * Reset all counter values to 0 and program the counters to send
-     * TIME_SLICE_SIGNAL when 'ticks_period' tick events have elapsed. (In reality
-     * the hardware triggers its interrupt some time after that.)
-     * This must be called while the task is stopped, and it must be called
-     * before the task is allowed to run again.
-     */
-    void reset(Ticks ticks_period);
-
-    /**
-     * Close the perfcounter fds. They will be automatically reopened if/when
-     * reset is called again.
-     */
-    void stop();
-
-    /**
-     * Read the current value of the ticks counter.
-     */
-    Ticks read_ticks();
-    Ticks read_ticks_nondestructively();
-
-    /**
-     * Adjust the LWP counter to account for additional instructions
-     */
-    void adjust(Ticks);
-
-    /* This choice is fairly arbitrary; linux doesn't use SIGSTKFLT so we
-     * hope that tracees don't either. */
-    enum { TIME_SLICE_SIGNAL = SIGSTKFLT };
-
-    static const struct perf_event_attr& ticks_attr();
-
-    struct lwpcb {
-      u32 flags;
-      u32 buffer_size;
-      u64 buffer_base;
-      u32 buffer_head_offset;
-      u32 rsvd20;
-      u64 missed_events;
-      u32 threshold;
-      u32 filters;
-      u64 base_ip;
-      u64 limit_ip;
-      u64 rsvd56;
-      u32 buffer_tail_offset;
-      u32 rsvd68;
-      u64 rsvd70;
-      struct {
-        u32 interval;
-        u32 counter;
-      } event[3];
-    };
-
-    struct lwp_xsave {
-      u64 lwpcb_address;
-      u32 flags;
-      u32 buffer_head_offset;
-      u64 buffer_base;
-      u32 buffer_size;
-      u32 filters;
-      u64 saved_event_record[4];
-      u32 event_counter[3];
-    };
-
-private:
-    rr::Task* task;
-    pid_t tid;
-    ScopedFd fd_ticks;
-    bool started;
-    unsigned long last_ticks_period;
-    unsigned long saved_ticks;
-    unsigned long ticks_read;
-    struct lwpcb lwpcb;
-    struct lwp_xsave xsave;
-  };
-}
-
-static void LWP::init_lwpcb(remote_ptr<void> buffer, size_t buffer_size)
+void LWP::init_buffer(remote_ptr<void> buffer, size_t buffer_size)
 {
   assert(buffer);
   assert(buffer_size >= 32 * 32);
@@ -96,47 +55,63 @@ static void LWP::init_lwpcb(remote_ptr<void> buffer, size_t buffer_size)
   memset(&lwpcb, 0, sizeof lwpcb);
   lwpcb.flags = LWP_FLAGS;
   lwpcb.buffer_size = buffer_size;
-  lwpcb.buffer_base = uintptr_t(buffer);
+  lwpcb.buffer_base = buffer.as_int();
+  lwpcb.filters = LWP_FILTERS;
   lwpcb.event[LWP_EVENT].interval = LWP_INTERVAL;
+
+  memset(&xsave, 0, sizeof xsave);
+  xsave.flags = LWP_FLAGS;
+  xsave.buffer_size = buffer_size;
+  xsave.buffer_base = buffer.as_int();
+  xsave.filters = LWP_FILTERS;
 }
 
-static bool LWP::write_lwpcb(remote_ptr<lwpcb> dest_lwp)
+bool LWP::write_lwpcb(remote_ptr<struct lwpcb> dest_lwp)
 {
-  remote_ptr<long> dest = remote_ptr<long>(dest_lwp);
+  remote_ptr<long> dest = remote_ptr<long>(dest_lwp.as_int());
   long *src = (long *)&lwpcb;
   assert(task);
   assert(dest);
 
-  int i;
+  unsigned int i;
   for (i = 0; i < (sizeof(lwpcb) + 7) / 8; i++) {
-    if (!task->ptrace_if_alive(PTRACE_POKEDATA, dest + i, src[i]))
+    if (!task->ptrace_if_alive(PTRACE_POKEDATA, dest + i, &src[i]))
       return false;
   }
 
   return true;
 }
 
-static bool LWP::read_lwp_xsave()
+unsigned LWP::xsave_area_size = 0;
+unsigned LWP::xsave_lwp_size = 0;
+unsigned LWP::xsave_lwp_off = 0;
+
+bool LWP::read_lwp_xsave()
 {
-  char *buf = malloc(xsave_area_size);
+  char *buf = (char *)malloc(xsave_area_size);
   if (!buf)
     return false;
   struct iovec vec = { buf, xsave_area_size };
   if (!task->ptrace_if_alive(PTRACE_GETREGSET, NT_X86_XSTATE, &vec))
     return false;
-  if (vec.size != xsave_area_size)
+  if (vec.iov_len != xsave_area_size)
     return false;
   memcpy(&xsave, buf, sizeof xsave);
+  free(buf);
+
+  return true;
 }
 
-static bool LWP::lwp_xsave_to_lwpcb()
+bool LWP::lwp_xsave_to_lwpcb()
 {
-  lwpcb.flags = xsave.flags;
-  lwpcb.buffer_size = xsave.buffer_size;
-  lwpcb.buffer_base = xsave.buffer_base;
-  lwpcb.buffer_head_offset = xsave.buffer_head_offset;
-  lwpcb.filters = xsave.filters;
-  lwpcb.event[LWP_EVENT].counter = xsave.event_counter[LWP_EVENT];
+  if (xsave.flags) {
+    lwpcb.flags = xsave.flags;
+    lwpcb.buffer_size = xsave.buffer_size;
+    lwpcb.buffer_base = xsave.buffer_base;
+    lwpcb.buffer_head_offset = xsave.buffer_head_offset;
+    lwpcb.filters = xsave.filters;
+    lwpcb.event[LWP_EVENT].counter = xsave.event_counter[LWP_EVENT];
+  }
 
   return true;
 }
@@ -147,6 +122,29 @@ LWP::LWP(Task *task, pid_t tid)
 {
   memset(&lwpcb, 0, sizeof lwpcb);
   memset(&xsave, 0, sizeof xsave);
+
+  if (xsave_area_size <= 0) {
+    auto cpuid_data = cpuid(CPUID_GETFEATURES, 0);
+    if (!(cpuid_data.ecx & (1 << 26))) {
+      abort();
+      // XSAVE not present
+      return;
+    }
+
+    // We'll use the largest possible area all the time
+    // even when it might not be needed. Simpler that way.
+    cpuid_data = cpuid(CPUID_GETXSAVE, 0);
+    xsave_area_size = cpuid_data.ecx;
+    long xsave_features = ((long)cpuid_data.edx << 32LL) + cpuid_data.eax;
+
+    if (xsave_features & (1LL<<62)) {
+      cpuid_data = cpuid(CPUID_GETXSAVE, 62);
+      xsave_lwp_size = cpuid_data.eax;
+      xsave_lwp_off = cpuid_data.ebx;
+    } else {
+      abort();
+    }
+  }
 }
 
 LWP::~LWP()
@@ -155,11 +153,34 @@ LWP::~LWP()
     fd_ticks.close();
 }
 
+static ScopedFd start_ticks(pid_t tid, int group_fd, struct perf_event_attr* attr)
+{
+  int fd = syscall(__NR_perf_event_open, attr, tid, -1, group_fd, 0);
+  if (0 > fd) {
+    if (errno == EACCES) {
+      FATAL() << "Permission denied to use 'perf_event_open'; are perf events "
+                 "enabled? Try 'perf record'.";
+    }
+    if (errno == ENOENT) {
+      FATAL() << "Unable to open performance counter with 'perf_event_open'; "
+                 "are perf events enabled? Try 'perf record'.";
+    }
+    FATAL() << "Failed to initialize counter";
+  }
+  if (ioctl(fd, PERF_EVENT_IOC_ENABLE, 0)) {
+    FATAL() << "Failed to start counter";
+  }
+  return fd;
+}
+
+static struct perf_event_attr ticks_attr;
 void LWP::reset(Ticks ticks_period)
 {
   assert(ticks_period >= 0);
   assert(ticks_period <= 0xffffff);
   assert(ticks_period <= LWP_INTERVAL);
+
+  start_ticks(tid, -1, &rr::ticks_attr);
 
   xsave.event_counter[LWP_EVENT] = ticks_period;
   lwpcb.event[LWP_EVENT].counter = ticks_period;
@@ -175,7 +196,7 @@ void LWP::stop()
 
 Ticks LWP::read_ticks_nondestructively()
 {
-  s64 counter_value = lwpcb.event[LWP_EVENT].counter;
+  long counter_value = lwpcb.event[LWP_EVENT].counter;
   if (counter_value & LWP_SIGN)
     counter_value |= LWP_SIGN;
   assert(counter_value >= -2048);
@@ -196,7 +217,7 @@ Ticks LWP::read_ticks()
 
 void LWP::adjust(Ticks diff)
 {
-  ticks -= diff;
+  ticks_read += diff;
 }
 
 static void init_perf_event_attr(struct perf_event_attr* attr,
@@ -209,10 +230,10 @@ static void init_perf_event_attr(struct perf_event_attr* attr,
   // only.
   attr->exclude_kernel = 1;
   attr->exclude_guest = 1;
+  attr->sample_period = 1; /* this is in interrupts */
 }
 
 static bool attributes_initialized;
-static struct perf_event_attr ticks_attr;
 
 static void init_attributes() {
   if (attributes_initialized) {
@@ -230,10 +251,12 @@ static void init_attributes() {
 
   fclose(f);
 
-  init_perf_event_attr(&ticks_attr, (perf_type_id)type, 0);
+  init_perf_event_attr(&rr::ticks_attr, (perf_type_id)type, 0);
 }
 
-const struct perf_event_attr& PerfCounters::ticks_attr() {
+const struct perf_event_attr& LWP::ticks_attr() {
   init_attributes();
   return rr::ticks_attr;
+}
+
 }
--- src/LWP.h
+++ src/LWP.h
@@ -1,5 +1,14 @@
 #ifndef RR_LWP_H_
 #define RR_LWP_H_
+
+#include <signal.h>
+#include <stdint.h>
+#include <sys/types.h>
+
+#include "remote_ptr.h"
+#include "Ticks.h"
+#include "ScopedFd.h"
+
 /* 0x8000000 - LwpInt: threshold interrupt
  * 0x4000000 - LwpPTSC: performance time stamp counter in event record
  * 0x2000000 - LwpCont: continuous mode sampling. This is required(?!).
@@ -7,18 +16,42 @@
 #define LWP_FLAGS 0x80000008L
 #define LWP_EVENT          2L
 #define LWP_FILTER 0x00000000L
+#define LWP_FILTERS 0x00000000L
 #define LWP_OFFSET 0
 #define LWP_INTERVAL 0x1ffffffL
 #define LWP_SIGN 0xfffffffff1000000L
+
+struct perf_event_attr;
+
 namespace rr {
 
   class Task;
 
-  
+  struct lwpcb {
+    uint32_t flags;
+    uint32_t buffer_size;
+    uint64_t buffer_base;
+    uint32_t buffer_head_offset;
+    uint32_t rsvd20;
+    uint64_t missed_events;
+    uint32_t threshold;
+    uint32_t filters;
+    uint64_t base_ip;
+    uint64_t limit_ip;
+    uint64_t rsvd56;
+    uint32_t buffer_tail_offset;
+    uint32_t rsvd68;
+    uint64_t rsvd70;
+    struct {
+      uint32_t interval;
+      uint32_t counter;
+    } event[3];
+  };
+
   class LWP {
   public:
     LWP(Task* task, pid_t tid);
-    ~LWP() {}
+    ~LWP();
 
     /**
      * Reset all counter values to 0 and program the counters to send
@@ -42,38 +75,31 @@ namespace rr {
     Ticks read_ticks_nondestructively();
 
     int ticks_fd() const { return fd_ticks.get(); }
-    
+
     /**
      * Adjust the LWP counter to account for additional instructions
      */
     void adjust(Ticks);
 
+    void init_buffer(remote_ptr<void> buffer, size_t buffer_size);
+
+    bool write_lwpcb(remote_ptr<lwpcb> dest_lwp);
+
+    bool read_lwp_xsave();
+
+    bool lwp_xsave_to_lwpcb();
+
+    void init_xsave(unsigned int area_size, unsigned int lwp_off, unsigned int lwp_size) {
+      xsave_area_size = area_size;
+      xsave_lwp_off = lwp_off;
+      xsave_lwp_size = lwp_size;
+    }
+    
     /* This choice is fairly arbitrary; linux doesn't use SIGSTKFLT so we
      * hope that tracees don't either. */
     enum { TIME_SLICE_SIGNAL = SIGSTKFLT };
 
     static const struct perf_event_attr& ticks_attr();
-
-    struct lwpcb {
-      uint32_t flags;
-      uint32_t buffer_size;
-      uint64_t buffer_base;
-      uint32_t buffer_head_offset;
-      uint32_t rsvd20;
-      uint64_t missed_events;
-      uint32_t threshold;
-      uint32_t filters;
-      uint64_t base_ip;
-      uint64_t limit_ip;
-      uint64_t rsvd56;
-      uint32_t buffer_tail_offset;
-      uint32_t rsvd68;
-      uint64_t rsvd70;
-      struct {
-        uint32_t interval;
-        uint32_t counter;
-      } event[3];
-    };
 
     struct lwp_xsave {
       uint64_t lwpcb_address;
@@ -96,6 +122,9 @@ private:
     unsigned long ticks_read;
     struct lwpcb lwpcb;
     struct lwp_xsave xsave;
+    static unsigned int xsave_area_size;
+    static unsigned int xsave_lwp_off;
+    static unsigned int xsave_lwp_size;
   };
 }
 #endif /* RR_LWP_H_ */
