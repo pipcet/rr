88021c8c2f7c760963b1ea7e5e73165acb9fb11b
Pip Cet <pipcet@gmail.com>
2016-07-14 14:31:04 +0000

    just in case it's actually the orig_ax field that's relevant...

--- src/Registers.h
+++ src/Registers.h
@@ -292,7 +292,7 @@ public:
   void set_flags(uintptr_t value);
   bool singlestep_flag() { return flags() & X86_TF_FLAG; }
   void clear_singlestep_flag() { set_flags(flags() & ~X86_TF_FLAG); }
-  void clear_resume_flag() { set_flags(flags() & ~X86_RF_FLAG); }
+  void clear_resume_flag() { set_flags(flags() & ~X86_RF_FLAG); if ((signed long)u.x64regs.orig_rax < -1) u.x64regs.orig_rax = 0; }
   bool df_flag() const { return flags() & X86_DF_FLAG; }
 
   // End of X86-specific stuff
