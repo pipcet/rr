e33ed59dc4896c00c9fb1d6e26db715ca310df2f
Pip Cet <pipcet@gmail.com>
2016-07-18 12:31:44 +0000

    diff reduction

--- src/RecordSession.cc
+++ src/RecordSession.cc
@@ -409,11 +409,9 @@ void RecordSession::task_continue(const StepState& step_state) {
 
   TicksRequest ticks_request;
   ResumeRequest resume;
-  bool update_syscall_state = false;
   if (step_state.continue_type == CONTINUE_SYSCALL) {
     ticks_request = RESUME_NO_TICKS;
     resume = RESUME_SYSCALL;
-    update_syscall_state = true;
   } else {
     if (t->has_stashed_sig(PerfCounters::TIME_SLICE_SIGNAL)) {
       // timeslice signal already stashed, no point in generating another one
